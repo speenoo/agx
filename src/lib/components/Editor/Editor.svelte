61d196420cc84dc77d0120cae3a68940f1a3ba29
Yann Amsellem <yann@agnostic.engineering>
2024-11-21 17:34:57 +0100

    chore(editor): add codemirror

---
+++ src/lib/components/Editor/Editor.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { sql } from '@codemirror/lang-sql';
+	import { EditorState } from '@codemirror/state';
+	import { EditorView, keymap, placeholder } from '@codemirror/view';
+	import { untrack } from 'svelte';
+	import './codemirror.css';
+	import { default_extensions, default_keymaps } from './extensions';
+	import { ProxyDialect } from './SQLDialect';
+
+	type Props = {
+		value: string;
+		onExec?: () => unknown;
+	};
+
+	let { value = $bindable(''), onExec }: Props = $props();
+
+	let container: HTMLDivElement;
+	let editor_view: EditorView;
+
+	$effect(() => {
+		editor_view = new EditorView({ parent: container });
+
+		const state = EditorState.create({
+			doc: untrack(() => value),
+			extensions: [
+				...default_extensions,
+				default_keymaps,
+				EditorView.darkTheme.of(true),
+				sql({ dialect: ProxyDialect }),
+				EditorView.updateListener.of((update) => {
+					if (update.docChanged) {
+						value = update.state.doc.toString();
+					}
+				}),
+				placeholder('Enter a query...'),
+				keymap.of([
+					{
+						key: 'Mod-Enter',
+						run: () => {
+							onExec?.();
+							return true;
+						}
+					}
+				])
+			]
+		});
+
+		editor_view.setState(state);
+
+		return () => editor_view.destroy();
+	});
+</script>
+
+<div bind:this={container}></div>
+
+<style>
+	div {
+		width: 100%;
+		height: 100%;
+		padding: 7px 2px;
+	}
+</style>
---
+++ src/lib/components/Editor/theme.ts
@@ -0,0 +1,41 @@
+import { HighlightStyle, syntaxHighlighting } from '@codemirror/language';
+import { tags as t } from '@lezer/highlight';
+
+export const theme = syntaxHighlighting(
+  HighlightStyle.define([
+    { tag: t.keyword, color: 'hsl(204, 88%, 65%)' },
+    {
+      tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+      color: 'hsl(45, 7%, 75%)'
+    },
+    { tag: [t.function(t.variableName), t.labelName], color: 'hsl(19, 67%, 75%)' },
+    { tag: [t.color, t.constant(t.name), t.standard(t.name)], color: 'hsl(45, 7%, 75%)' },
+    { tag: [t.definition(t.name), t.separator], color: 'hsl(45, 7%, 75%)' },
+    {
+      tag: [
+        t.typeName,
+        t.className,
+        t.number,
+        t.changed,
+        t.annotation,
+        t.modifier,
+        t.self,
+        t.namespace
+      ],
+      color: 'hsl(19, 67%, 75%)'
+    },
+    {
+      tag: [t.operator, t.operatorKeyword, t.url, t.escape, t.regexp, t.link, t.special(t.string)],
+      color: 'hsl(45, 7%, 75%)'
+    },
+    { tag: [t.meta, t.comment], color: 'hsl(0, 0%, 55%)' },
+    { tag: t.strong, fontWeight: 'bold' },
+    { tag: t.emphasis, fontStyle: 'italic' },
+    { tag: t.strikethrough, textDecoration: 'line-through' },
+    { tag: t.link, color: 'hsl(45, 7%, 75%)', textDecoration: 'underline' },
+    { tag: t.heading, fontWeight: 'bold', color: 'hsl(220deg 2% 90%)' },
+    { tag: [t.atom, t.bool], color: 'hsl(45, 7%, 75%)' },
+    { tag: [t.processingInstruction, t.string, t.inserted], color: 'hsl(41, 37%, 68%)' },
+    { tag: t.invalid, color: '#ff008c' }
+  ])
+);
