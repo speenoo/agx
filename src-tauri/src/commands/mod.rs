2671e321204aaba2ee75f66536e2bd011c4bd256
didierfranc <didier@agnostic.engineering>
2024-12-13 16:55:38 +0100

    feat: support udfs
    sql and xml should be placed into /user_defined and bin in /bin

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,21 +1,25 @@
 use crate::chdb::{arg::Arg, format, lib};
 
+use std::borrow::Cow;
 use std::sync::Mutex;
 use tauri::State;
 
-use crate::AppState;
+use crate::{chdb, AppState};
 use String;
 
 #[tauri::command]
 pub async fn query(query: String, state: State<'_, Mutex<AppState>>) -> Result<String, String> {
     let state = state.lock().unwrap();
+    let config_path = state.clickhouse_dir.join("config.xml".to_string());
 
     let args = vec![
+        // Arg::LogLevel(chdb::log_level::LogLevel::Trace),
         Arg::OutputFormat(format::OutputFormat::JSON),
         Arg::Custom(
             "path".into(),
             Some(state.clickhouse_dir.display().to_string().into()),
         ),
+        Arg::ConfigFilePath(Cow::Borrowed(config_path.to_str().unwrap())),
     ];
 
     return match lib::execute(&query, Some(&args)) {
