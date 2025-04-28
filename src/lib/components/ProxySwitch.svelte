69581b77bbc63538fbb3674856a8c7914c4ed84b
Yann Amsellem <yann@agnostic.engineering>
2025-04-28 10:27:12 +0200

    fix(native): save and restore proxy state

--- src/lib/components/ProxySwitch.svelte
+++ src/lib/components/ProxySwitch.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import { onMount } from 'svelte';
 
 	const usingProxy = $derived(page.url.searchParams.has('proxy'));
 	async function setProxy(proxy?: string) {
 		const url = new URL(page.url);
 		url.searchParams.delete('proxy');
 		if (proxy) url.searchParams.set('proxy', proxy);
+		save(proxy);
 		await goto(url, { replaceState: true });
 		window.location.reload();
 	}
+
+	function save(proxy?: string) {
+		if (proxy) localStorage.setItem('agx:proxy', proxy);
+		else localStorage.removeItem('agx:proxy');
+	}
+
+	onMount(() => {
+		const queryParamsProxy = page.url.searchParams.get('proxy') ?? undefined;
+		const storageProxy = localStorage.getItem('agx:proxy') ?? undefined;
+		if (queryParamsProxy !== storageProxy) setProxy(storageProxy);
+	});
 </script>
 
 <div>
